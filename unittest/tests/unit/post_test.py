eae0e088129444a7fd6449a5e4e1962716afccb9
Ashish Anand <anandashish456@gmail.com>
2019-04-23 19:16:44 +0530

    Code push for unittest

---
+++ unittest/tests/unit/post_test.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+from post import Post
+
+
+class PostTest(TestCase):
+    def test_create_obj(self):
+        obj = Post("Test", "Cont")
+
+        self.assertEqual('Test', obj.title)
+        self.assertEqual('Cont', obj.content)
+
+    def test_json(self):
+        obj = Post("Test", "Cont")
+        self.assertDictEqual({'Title': 'Test', 'Content': 'Cont'}, obj.json())
+
